824b729fa63cf693f76bef2ac880850d046dc9f6
markaozx <markaoxz@gmail.com>
2025-08-05 14:10:26 -0300

    puxando nome digitado

--- Desenvolvimento Back-End 2/Pokedex/script.js
+++ Desenvolvimento Back-End 2/Pokedex/script.js
@@ -1,6 +1,9 @@
 document.getElementById('search-form').addEventListener('submit', function(event){
     event.preventDefault();
     
+    const form = event.target;
+    const pokemonNome = form.elements['pokemon_name'].value;
+    console.log(pokemonNome);
 
     const imagem = document.getElementById('pokemon-image')
     const nome = document.getElementById('pokemon-name')
