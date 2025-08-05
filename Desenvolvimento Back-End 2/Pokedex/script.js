827ea00f238f079fd7d3629b6107c10dbbf6e5e3
markaozx <markaoxz@gmail.com>
2025-08-05 14:03:00 -0300

    d

--- Desenvolvimento Back-End 2/Pokedex/script.js
+++ Desenvolvimento Back-End 2/Pokedex/script.js
@@ -1,4 +1,11 @@
 document.getElementById('search-form').addEventListener('submit', function(event){
     event.preventDefault();
-    console.log("Pikachu")
+    
+
+    const imagem = document.getElementById('pokemon-image')
+    const nome = document.getElementById('pokemon-name')
+    const id = document.getElementById('pokemon-id')
+    const tipo = document.getElementById('pokemon-type')
+    const desc = document.getElementById('pokemon-description')
+    
 })
